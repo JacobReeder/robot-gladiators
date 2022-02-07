e3cff8f72fafa477030fa7d1fc0ff850c6cc639b
JacobReeder <jacob.reeder1@gmail.com>
2022-02-06 19:05:28 -0800

    js sheet variables

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+   //This will do math
+   console.log(10+10);
+   console.log("Our robots name is " +playerName);
+
+var name = "your-name";
+console.log(name);
+
+function fight() {
+   window.alert("The fight has begun");
+}
+
+// fight();
